b000e493e37548c43d5e6bea3f3247db20c087d6
Boris Horowitz <boris_atw@yahoo.fr>
2014-12-09 18:27:21 +0100

    Final Data Management

--- python/6.unep_final_data_management.py
+++ python/6.unep_final_data_management.py
@@ -39,15 +39,32 @@ sql += """
 	CREATE TABLE public.data_ranking AS
 		SELECT
 			*
-			, ntile(10) OVER (PARTITION BY id_variable ORDER BY id_variable, value) AS ranking
+			, ntile(10) OVER (PARTITION BY id_variable ORDER BY id_variable, value) AS rating
+			, rank() over (PARTITION BY id_variable ORDER BY id_variable, value) AS ranking
+			, '#FFFFFF'::character varying(7) AS color
 		FROM
 			data_last_year
 		ORDER BY
 			id_variable, value
+	;
 """
 
 ### Création des index
 sql += 'ALTER TABLE data_ranking ADD CONSTRAINT pk_data_ranking PRIMARY KEY(iso_2, id_variable);'
+
+### Mise à jour des codes couleur
+sql += """
+	UPDATE public.data_ranking set color = '#E5000A' WHERE rating = 1;
+	UPDATE public.data_ranking set color = '#E12902' WHERE rating = 2;
+	UPDATE public.data_ranking set color = '#DE5B03' WHERE rating = 3;
+	UPDATE public.data_ranking set color = '#DA8B05' WHERE rating = 4;
+	UPDATE public.data_ranking set color = '#D7B807' WHERE rating = 5;
+	UPDATE public.data_ranking set color = '#C4D309' WHERE rating = 6;
+	UPDATE public.data_ranking set color = '#94D00B' WHERE rating = 7;
+	UPDATE public.data_ranking set color = '#67CC0C' WHERE rating = 8;
+	UPDATE public.data_ranking set color = '#3CC90E' WHERE rating = 9;
+	UPDATE public.data_ranking set color = '#13C60F' WHERE rating = 10;
+"""
 
 ### Exécution de la requete et commit général
 cur.execute(sql)
